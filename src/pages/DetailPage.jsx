cfaffabdf145e5b1eaa19f8d66a4bd3d76fba3d3
jlvilmak <jlvilmak@student.ubc.ca>
2024-03-09 18:00:58 -0800

    Detail Page

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,8 +1,33 @@
+import Card from "../components/home/Card";
+import udemyImage from "../images/udemy.png";
+
 function DetailPage() {
   return (
     <div className="pt-20 px-10">
       <div className="">Detail page</div>
+      <div> 
+        <header>
+          <h1>
+            Business & Management
+          </h1>
+        </header>
+      </div>
+
+      <div className="flex flex-wrap gap-2 justify-center py-20">
+        <div>
+          <Card title={"Udemy: Succeed in Business Even if you Don't Know Where to Start!"} 
+          imgPath={udemyImage} 
+          link={"https://www.udemy.com/course/succeed-in-business-even-if-you-dont-know-where-to-start/"}/>
+        </div>
+
+        <div>
+          <Card title={"Udemy: Business Analysis Essential Skills Training"} 
+          imgPath={udemyImage} 
+          link={"https://www.udemy.com/course/business-analysis-essential-skills-training/"}/>
+        </div>
+      </div>
     </div>
+    
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
 import Card from "../components/home/Card";
 import businessImage from "../images/Business.png";
+import compsciImage from "../images/Comp sci.png";
+import artsImage from "../images/Arts & Humanities.png";
+import dataImage from "../images/Data.png";
+import literatureImage from "../images/Literature.png";
+import mathImage from "../images/Mathematics.png";
+import socialImage from "../images/Social science.jpg";
 
 function Home() {
   // const subject = [
@@ -44,15 +50,13 @@ function Home() {
   return (
     <div className="pt-20 px-10">
       <div className="flex flex-wrap gap-2 justify-center py-20">
-        <Card title={"Business & Management"} imgPath={businessImage} />
-        <Card title={"Business & Management"} imgPath={businessImage} />
-        <Card title={"Business & Management"} imgPath={businessImage} />
-        <Card title={"Business & Management"} imgPath={businessImage} />
-        <Card title={"Business & Management"} imgPath={businessImage} />
-        <Card title={"Business & Management"} imgPath={businessImage} />
-        <Card title={"Business & Management"} imgPath={businessImage} />
-        <Card title={"Business & Management"} imgPath={businessImage} />
-        <Card title={"Business & Management"} imgPath={businessImage} />
+        <Card title={"Business & Management"} imgPath={businessImage} link={"/detail-page"}/>
+        <Card title={"Arts & Humanities"} imgPath={artsImage} link={"/detail-page"}/>
+        <Card title={"Computer Science"} imgPath={compsciImage} link={"/detail-page"}/>
+        <Card title={"Data Analytics"} imgPath={dataImage} link={"/detail-page"}/>
+        <Card title={"Language & Literature"} imgPath={literatureImage} link={"/detail-page"}/>
+        <Card title={"Mathematics"} imgPath={mathImage} link={"/detail-page"}/>
+        <Card title={"Social Sciences"} imgPath={socialImage} link={"/detail-page"}/>
       </div>
     </div>
   );
