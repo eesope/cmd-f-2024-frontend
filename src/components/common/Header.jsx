3d0c7eef71ecc9003a864c23771d796db6999741
yuki-o1o5 <jkksst1712@gmail.com>
2024-03-10 10:47:04 -0700

    feat: create curiculum

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,12 @@
+import gnosisImage from "../../images/cmd-f_gnosis_logo.png";
+
 function Header() {
   return (
     <div className="flex justify-between items-center h-20 fixed w-full px-10 bg-white shadow-sm">
-      <h1 className="text-2xl">Gnosis</h1>
+      <div className="flex items-center gap-2">
+        <img src={gnosisImage} alt="gnosis" className="h-12 w-12" />
+        <h1 className="text-2xl">Gnosis</h1>
+      </div>
       <nav>
         <ul className="flex gap-6">
           <li>
--- src/components/curriculum/CurriculumCard.jsx
+++ src/components/curriculum/CurriculumCard.jsx
@@ -1,8 +1,42 @@
-function CurriculumCard({ title, subject }) {
+function CurriculumCard({
+  title,
+  subject,
+  url,
+  startDate,
+  endDate,
+  status,
+  // handleEdit,
+}) {
   return (
-    <div className="w-full rounded-lg p-4 border-gray-300 border-2">
-      <div className="text-gray-900 font-bold mb-2">{title}</div>
-      <div className="text-gray-900 font-bold mb-2">{subject}</div>
+    <div className="w-full rounded-lg p-4 shadow-md">
+      <div className="text-gray-00 font-bold text-lg mb-2">{title}</div>
+      <div className="text-yellow-500 mb-2">
+        Subject:<span className="text-gray-500 pl-2">{subject}</span>
+      </div>
+      <div className="text-yellow-500 mb-2">
+        URL:<span className="text-gray-500 pl-2">{url}</span>
+      </div>
+      <div className="text-yellow-500 mb-2">
+        Start Date: <span className="text-gray-500 pl-2">{startDate}</span>
+      </div>
+      <div className="text-yellow-500 mb-2">
+        End Date:<span className="text-gray-500 pl-2">{endDate}</span>
+      </div>
+      {status === "true" ? (
+        <div className="text-yellow-500 mb-2">
+          Study Status: <span className="text-gray-500 pl-2">Completed</span>
+        </div>
+      ) : (
+        <div className="text-yellow-500 mb-2">
+          Study Status: <span className="text-gray-500 pl-2">Ongoing</span>
+        </div>
+      )}
+      {/* <button
+        className="justify-center rounded-md border border-transparent bg-yellow-100 px-4 py-2 text-sm font-medium text-yellow-900 hover:bg-yellow-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-yellow-500 focus-visible:ring-offset-2"
+        onClick={handleEdit}
+      >
+        Edit
+      </button> */}
     </div>
   );
 }
